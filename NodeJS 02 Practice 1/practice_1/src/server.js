3d784a4d3d0600832335d90c53a76eeb71fe7ff6
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-05-26 20:24:49 +0200

    practice 1

--- NodeJS 02 Practice 1/practice_1/src/server.js
+++ NodeJS 02 Practice 1/practice_1/src/server.js
@@ -3,12 +3,29 @@ import http from "node:http";
 const server = http.createServer((request, response) => {
     switch (request.url) {
         case "/":
-            return response.end("<h1>Welcome to the Home Page!</h1>")
+            response.statusCode = 200;
+            response.end("<h1>Welcome to the Home Page!</h1>");
+            break;
         case "/about":
-            return response.end("<h1>About Us</h1>")
+            response.statusCode = 200;
+            response.end("<h1>About Us</h1>");
+            break;
         default:
-            return response.end("<h1>404 Not Found</h1>");
+            response.statusCode = 404;
+            response.end("<h1>404 Not Found</h1>");
+            break;
     }
 });
 
-server.listen(3000, () => console.log("Server = port: 3000"));
+// const server = http.createServer((request, response) => {
+//     switch (request.url) {
+//         case "/":
+//             return response.end("<h1>Welcome to the Home Page!</h1>")
+//         case "/about":
+//             return response.end("<h1>About Us</h1>")
+//         default:
+//             return response.end("<h1>404 Not Found</h1>");
+//     }
+// });
+
+server.listen(3000, () => console.log("Server started on port: 3000"));

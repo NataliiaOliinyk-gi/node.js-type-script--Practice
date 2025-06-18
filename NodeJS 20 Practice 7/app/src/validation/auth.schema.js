99d580c21cf6c6363a0640b24fe5fc824ef9d2c6
Nataliia Oliinyk <nataliia.oliinyk.de@gmail.com>
2025-06-18 14:43:38 +0200

    Node.js practice add ReadMe.md

--- NodeJS 20 Practice 7/app/src/validation/auth.schema.js
+++
@@ -1,8 +0,0 @@
-import * as Yup from "yup";
-
-import { emailSchema, passwordSchema } from "./users.schema.js";
-
-export const loginSchema = Yup.object({
-    email: emailSchema,
-    password: passwordSchema,
-});
